587cc3783a15ca08a8a07304cb995b4758f5927a
Oscar Welton-Rosie <185318677+OWelton-Rosie@users.noreply.github.com>
2025-07-20 17:04:59 +1200

    Update errors.js

--- src/scripts/errors.js
+++ src/scripts/errors.js
@@ -1,7 +1,7 @@
 export const ERRORS = {
-  fetchFail: "Could not load stationery list. Please try again later.",
+  fetchFail: "Could not load stationery list. Please try again later while we resolve this issue.",
   noSubjects: "You must select at least one subject.",
   tooManyLanguages: "You may only select one language: Japanese, Chinese, or Spanish.",
   tooManySubjects: "You can select a maximum of 6 subjects.",
-  tooManySubjectsYr10: "Year 10 students must take 6 required subjects, plus either 1 language and 2 more subjects, or 4 subjects if no language is chosen.",
+  tooManySubjectsYr10: "Year 10 students must take 5 required subjects, plus either 1 language and 2 more subjects, or 4 subjects if no language is chosen.",
 };
